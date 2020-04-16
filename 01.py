f6286c4b3298de63135030de90b48fde0aad5617
Njhlx <34767657+Njhlx@users.noreply.github.com>
2020-04-16 14:33:51 +0800

    Update 01.py

--- 01.py
+++ 01.py
@@ -6,8 +6,8 @@ from aliyunsdkecs.request.v20140526 import DescribeInstancesRequest
 from aliyunsdkecs.request.v20140526 import StopInstanceRequest
 # 创建AcsClient实例
 client = AcsClient(
-   "LTAI7G0ILr3MeK78",
-   "iFMvVjrokyM4cmI4TQ0xqY0Jcdz9Qi",
+   "@@@@",
+   "@@@@",
    "cn-beijing"
 );
 # 创建request，并设置参数
